5c5e73bbfd834a7da09acbbd4965ad7339e23eed
eoncarlyle <me@iainschmitt.com>
2025-03-09 09:26:08 -0500

    Working example of 3.1

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use std::sync::Arc;
-use std::thread;
+use std::thread::{self};
 mod lbs;
 
 fn main() {
-    let handle0 = thread::spawn(|| {
+    problem_3_1();
+}
+
+fn problem_3_1() {
+    let sem = Arc::new(lbs::Semaphore::new(0));
+    let sem_clone0 = Arc::clone(&sem);
+    let sem_clone1 = Arc::clone(&sem);
+
+    let handle0 = thread::spawn(move || {
         println!("Statement a1");
-        sem.release();
+        sem_clone0.release();
     });
 
-    let handle1 = thread::spawn(|| {
-        sem.acquire();
+    let handle1 = thread::spawn(move || {
+        sem_clone1.acquire();
         println!("Statement b1");
     });
 
