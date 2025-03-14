54d86e23e50f9d397e2fb1755eb0317541bf16c3
eoncarlyle <contact@iainschmitt.com>
2025-03-13 20:37:40 -0500

    F-sharp task question

--- f-sharp/Program.fs
+++ f-sharp/Program.fs
@@ -20,6 +20,16 @@ let problem_3_8_thread
                 Console.WriteLine $"Dancer: {dancer_list.Dequeue()}")
 
 
+let problem_3_8_task (internal_sem: Semaphore) (external_sem: Semaphore) (dancer_list: Queue<String>) (label: String) =
+    task {
+        while true do
+            if dancer_list.Count <> 0 then
+                Console.WriteLine $"{label} thread waiting"
+                internal_sem.Release() |> ignore
+                external_sem.WaitOne() |> ignore
+                Console.WriteLine $"Dancer: {dancer_list.Dequeue()}"
+    }
+
 let problem_3_8 () =
     let leader_sem = new Semaphore(0, 1)
     let follower_sem = new Semaphore(0, 1)
@@ -31,18 +41,18 @@ let problem_3_8 () =
     leader_list.Enqueue("leader3")
     leader_list.Enqueue("leader4")
 
-    let leaders = problem_3_8_thread leader_sem follower_sem leader_list "leader"
+    let leaders = problem_3_8_task leader_sem follower_sem leader_list "leader"
 
-    let follwers = problem_3_8_thread follower_sem leader_sem follow_list "follower"
+    let follwers = problem_3_8_task follower_sem leader_sem follow_list "follower"
+
+    follwers.Start()
+    leaders.Start()
 
     follow_list.Enqueue("follower1")
     follow_list.Enqueue("follower2")
     follow_list.Enqueue("follower3")
 
-    leaders.Start()
-    follwers.Start()
-    leaders.Join()
-    follwers.Join()
+    follwers.Wait()
     0
 
 problem_3_8 ()
