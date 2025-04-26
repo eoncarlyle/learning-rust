8c63938124c3d478591b4ffc4923b269b853eac8
eoncarlyle <me@iainschmitt.com>
2025-04-27 00:33:02 +0900

    Was stuck on this error, spot the issue here

--- f-sharp/Chapter4.fs
+++ f-sharp/Chapter4.fs
@@ -23,42 +23,27 @@ let problem_4_2_write_thread
 let problem_4_2_read_thread
     (label: int)
     (occupied: Semaphore)
-    (reader_count_mutex: Semaphore)
-    (active_readers: Ref<int>)
+    (lightswitch: Lightswitch)
     =
     Thread(fun () ->
         while true do
-            toggleSem reader_count_mutex Wait
-            if active_readers.Value = 0 then toggleSem occupied Wait
-            Console.WriteLine $"{label} reader writing {active_readers.Value + 1} to read_count"
-            active_readers.Value <- active_readers.Value + 1
-            toggleSem reader_count_mutex Release
-            
+            lightswitch.Lock occupied
             Console.WriteLine $"{label} reader reading"
             Thread.Sleep(300)
-            
-            toggleSem reader_count_mutex Wait
-            let switching_off = active_readers.Value = 1
-            active_readers.Value <- active_readers.Value - 1
-            if switching_off then
-                Console.WriteLine $"{label} reader last one out"
-                toggleSem occupied Release
-                
-            toggleSem reader_count_mutex Release
-    )
+            //Dropping the parenthesis off of the statement below
+            //F# parser limitations
+            lightswitch.Unlock occupied)
     
 let problem_4_2 =
     let reader_count = 2
     let writer_count = 2
     let occupied = new Semaphore(1, 1)
-    let reader_count_mutex = new Semaphore(1, 1)
-    let active_readers = ref 0
-    
-    let writers = [ 0..reader_count-1 ] |> List.map (fun i -> problem_4_2_write_thread i occupied)
-    let readers = [ 0..writer_count-1 ] |> List.map (fun i -> problem_4_2_read_thread i occupied reader_count_mutex active_readers)
+    let lightswitch = Lightswitch(0)
+   
+    let writers = [ 0..writer_count-1 ] |> List.map (fun i -> problem_4_2_write_thread i occupied)
+    let readers = [ 0..reader_count-1 ] |> List.map (fun i -> problem_4_2_read_thread i occupied lightswitch)
     writers |> List.iter _.Start()
     readers |> List.iter _.Start()
     
     writers |> List.iter _.Join()
     readers |> List.iter _.Join()
-    
--- f-sharp/Common.fs
+++ f-sharp/Common.fs
@@ -14,3 +14,20 @@ let toggleSem (sem: Semaphore) (semaphoreOperation: SemaphoreOperation) =
     match semaphoreOperation with
     | Release -> sem.Release() |> ignore
     | Wait -> sem.WaitOne() |> ignore
+
+type Lightswitch(counter, mutex) =
+    let mutable counter = counter
+    let mutex = mutex
+    
+    member this.Lock semaphore =
+        toggleSem mutex Wait
+        counter <- counter + 1
+        if counter = 1 then toggleSem semaphore Wait
+        toggleSem mutex Release
+        
+    member this.Unlock semaphore =
+        toggleSem mutex Wait
+        counter <- counter - 1
+        if counter = 0 then toggleSem semaphore Release
+        toggleSem mutex Release
+        
